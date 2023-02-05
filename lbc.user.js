db41653e55a26eae719c7b7c88e5e6a9ba914c5a
Jean Philippe Crevoisier <jpcrevoisier@gmail.com>
2023-02-05 16:15:10 +0100

    ...

--- lbc.user.js
+++ lbc.user.js
@@ -2,7 +2,8 @@
 // @name     Anonyme Script 508089
 // @version  1
 // @grant    none
-// @include https://www.leboncoin.fr/ventes_immobilieres/*
+// @include https://www.leboncoin.fr/*
+// @require https://raw.githubusercontent.com/jpcrevoisier/users.script/main/lbc.js
 // ==/UserScript==
 
 
@@ -10,4 +11,4 @@ firstScript=document.querySelector('script');
 newScript=document.createElement('script');
 newScript.type = 'text/javascript';
 newScript.src = 'https://raw.githubusercontent.com/jpcrevoisier/users.script/main/lbc.js';
-firstScript.parentNode.insertBefore(newScript, firstScript);
+// firstScript.parentNode.insertBefore(newScript, firstScript);
